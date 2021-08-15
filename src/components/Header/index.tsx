ffde862de9cb8fd8fa1d005da9226dfdeead2fea
Alvaro Neto <alvaro_neto96@hotmail.com>
2021-08-15 09:23:40 -0300

    Prismic CMS implemented

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import { ActiveLink } from '../ActiveLink/index';
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -7,10 +9,14 @@ export function Header() {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
 
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
+                </nav>
                 <SignInButton />
             </div>
         </header>
